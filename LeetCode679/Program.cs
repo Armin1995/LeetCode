ac4c68a28684eca77fff9a2bab96d00767cfa76e
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-09-30 11:12:16 +0800

    #679 24 点游戏  #701 二叉搜索树中的插入操作

---
+++ LeetCode679/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode679
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            bool is24 = solution.JudgePoint24(new int[] { 1, 2, 1, 2 });
+            Console.WriteLine(is24);
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public bool JudgePoint24(int[] nums)
+        {
+            if (nums == null || nums.Length != 4 || nums.Any(o => o == 0))
+            {
+                return false;
+            }
+
+            var doubleNums = new double[nums.Length];
+            nums.ToList().ConvertAll(o => (double)o).CopyTo(doubleNums);
+            return AX2(doubleNums);
+        }
+
+        public bool AX2(double[] nums)
+        {
+            var is24 = false;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var a = nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    var b = nums[j];
+                    foreach (var result in GetResultList(a, b))
+                    {
+                        double[] newArray = new double[nums.Length - 1];
+                        newArray[0] = result;
+                        if (newArray.Length == 1)
+                        {
+                            if (Math.Abs(newArray[0] - 24) < 0.01d)
+                            {
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            int index = 0;
+                            for (int k = 0; k < nums.Length; k++)
+                            {
+                                if (k == i || k == j)
+                                {
+                                    continue;
+                                }
+                                index++;
+                                newArray[index] = nums[k];
+                            }
+                            is24 = AX2(newArray);
+                            if (is24)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public List<double> GetResultList(double a, double b)
+        {
+            return new List<double>() { a + b, a - b, a * b, a / b };
+        }
+    }
+}
---
+++ LeetCode701/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root != null)
+            {
+                if (val > root.val)
+                {
+                    if (root.right != null)
+                    {
+                        InsertIntoBST(root.right, val);
+                    }
+                    else
+                    {
+                        root.right = new TreeNode(val);
+                    }
+                }
+                else if (val < root.val)
+                {
+                    if (root.left != null)
+                    {
+                        InsertIntoBST(root.left, val);
+                    }
+                    else
+                    {
+                        root.left = new TreeNode(val);
+                    }
+                }
+            }
+            else
+            {
+                root = new TreeNode(val);
+            }
+            return root;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
