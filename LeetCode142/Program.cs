e0e8ae6ebe6ba1caf7795048db5415b0dfebcdc1
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-10-10 11:32:13 +0800

    LeetCode142

---
+++ LeetCode142/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            if (head != null)
+            {
+                var tempList = new List<ListNode>() { head };
+                while (head.next != null)
+                {
+                    head = head.next;
+                    if (tempList.Contains(head))
+                    {
+                        return head;
+                    }
+                    else
+                    {
+                        tempList.Add(head);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
