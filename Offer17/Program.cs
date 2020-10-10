5ab68dafa6f83494cb78df7b8240c0c31abe168f
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-10-10 15:46:43 +0800

    Offer17

---
+++ Offer17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Offer17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int[] PrintNumbers(int n)
+        {
+            int count = (int)Math.Pow(10, n) - 1;
+            var array = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = i + 1;
+            }
+            return array;
+        }
+    }
+}
