f0754a5c951db395783e29e5fdbbb82548087379
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-10-10 14:52:02 +0800

    LeetCode754

---
+++ LeetCode754/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode754
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int ReachNumber(int target)
+        {
+            target = Math.Abs(target);
+            var sum = 0;
+            var result = 0;
+            while (sum < target)
+            {
+                result++;
+                sum += result;
+            }
+            var delta = sum - target;
+            if (delta % 2 == 0)
+            {
+                return result;
+            }
+            else
+            {
+                if (result % 2 == 0)
+                {
+                    return result + 1;
+                }
+                else
+                {
+                    return result + 2;
+                }
+            }
+        }
+    }
+}
