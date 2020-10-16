c4ad2da8ea181ebdd49cbca1e6d8a35131691c81
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-10-16 09:13:00 +0800

    LeetCode977

---
+++ LeetCode977/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode977
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            return A.Select(o => (int)Math.Pow(o, 2)).OrderBy(o => o).ToArray();
+        }
+    }
+}
