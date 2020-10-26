321afc15b00bef5dba3f7f60d16cebb549dfb34b
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-10-26 09:29:56 +0800

    LeetCode1365

---
+++ LeetCode1365/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode1365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            if (nums == null)
+            {
+                return null;
+            }
+            int[] result = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        result[i] = result[i] + 1;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
