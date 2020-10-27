0cc988bf88f358eda433a6e676fd23119a62f256
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-10-27 08:54:12 +0800

    LeetCode144

---
+++ LeetCode144/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            Preorder(root, result);
+            return result;
+        }
+
+        private void Preorder(TreeNode root, IList<int> result)
+        {
+            if (root != null)
+            {
+                result.Add(root.val);
+                Preorder(root.left, result);
+                Preorder(root.right, result);
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ LeetCode343/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode343
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            var result = s.IntegerBreak(58);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    class Solution
+    {
+        public int IntegerBreak(int n)
+        {
+            int[] dp = new int[n + 1];
+            for (int i = 2; i <= n; i++)
+            {
+                int curMax = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    curMax = Math.Max(curMax, Math.Max(j * (i - j), j * dp[i - j]));
+                }
+                dp[i] = curMax;
+            }
+            return dp[n];
+        }
+    }
+}
