11fced5e955e8d756c8cde5394e11bfe7d2eb1b5
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-10-30 10:44:23 +0800

    129 求根到叶子节点数字之和 463 岛屿的周长

---
+++ LeetCode129/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 129 求根到叶子节点数字之和
+/// </summary>
+namespace LeetCode129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            List<int> sums = new List<int>();
+            Sum(root, "", ref sums);
+            return sums.Sum();
+        }
+
+        public void Sum(TreeNode node, string combime, ref List<int> sums)
+        {
+            combime += node.val;
+
+            if (node.left == null && node.right == null)
+            {
+                sums.Add(int.Parse(combime));
+                combime = combime.Substring(0, combime.Length - 1);
+            }
+
+            if (node.left != null)
+            {
+                Sum(node.left, combime, ref sums);
+            }
+
+            if (node.right != null)
+            {
+                Sum(node.right, combime, ref sums);
+            }
+        }
+    }
+}
---
+++ LeetCode463/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 岛屿的周长
+/// </summary>
+namespace LeetCode463
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int IslandPerimeter(int[][] grid)
+        {
+            var landCount = 0;
+            var linkCount = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        landCount++;
+
+                        if (i != grid.Length - 1)
+                        {
+                            if (grid[i][j] == grid[i + 1][j])
+                            {
+                                linkCount++;
+                            }
+                        }
+
+                        if (j != grid[i].Length - 1)
+                        {
+                            if (grid[i][j] == grid[i][j + 1])
+                            {
+                                linkCount++;
+                            }
+                        }
+                    }
+                }
+            }
+            return landCount * 4 - linkCount * 2;
+        }
+    }
+}
