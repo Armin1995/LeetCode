1b9cfb13965bfa10e2c9b9a2f4883401c9ec0a42
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-11-02 17:22:30 +0800

    多做几题

---
+++ Interview16.24/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 数对和
+/// </summary>
+namespace Interview16._24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> PairSums(int[] nums, int target)
+        {
+            if (nums == null)
+            {
+                return null;
+            }
+            nums = nums.OrderBy(o => o).ToArray();
+            List<IList<int>> result = new List<IList<int>>();
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right];
+                if (sum < target)
+                {
+                    left++;
+                }
+                else if (sum > target)
+                {
+                    right--;
+                }
+                else
+                {
+                    result.Add(new List<int>() { nums[left], nums[right] });
+                    left++;
+                    right--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode371/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 两整数之和
+/// </summary>
+namespace LeetCode371
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int GetSum(int a, int b)
+        {
+            if (b == 0) return a;
+            return GetSum(a ^ b, (a & b) << 1);
+        }
+    }
+}
