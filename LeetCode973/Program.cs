3f148da38493402d57ed463e075c47e54a667a81
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-11-09 09:45:56 +0800

    973 最接近原点的 K 个点

---
+++ LeetCode973/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 最接近原点的 K 个点
+/// </summary>
+namespace LeetCode973
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Solution
+    {
+        public int[][] KClosest(int[][] points, int K)
+        {
+            if (points == null || points.Length == 0 || K == 0)
+            {
+                return null;
+            }
+            var indexDistanceDict = new Dictionary<int, double>();
+            for (int i = 0; i < points.Length; i++)
+            {
+                var distance = Math.Pow(points[i][0], 2) + Math.Pow(points[i][1], 2);
+                indexDistanceDict.Add(i, distance);
+            }
+            var indexList = indexDistanceDict.OrderBy(o => o.Value).Take(K).ToDictionary(o => o.Key, v => v.Value).Keys.ToList();
+            return indexList.ConvertAll(o => points[o]).ToArray();
+        }
+    }
+}
