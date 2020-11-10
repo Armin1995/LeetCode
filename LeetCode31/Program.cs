f4bc91b84fb570408a9e1993b95bb30e25646e80
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-11-10 17:58:47 +0800

    31 下一个排序

---
+++ LeetCode31/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public void NextPermutation(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                var current = nums[nums.Length - 1 - i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var last = nums[nums[nums.Length - 1 - j]];
+
+                }
+            }
+        }
+    }
+}
