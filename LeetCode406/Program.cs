6ad4514beeefa06e42c496579c196097ff31db7d
Armin君 <aichenzhupo127@outlook.com>
2020-11-17 00:11:57 +0800

    406 根据身高重建队列

--- LeetCode406/Program.cs
+++ LeetCode406/Program.cs
@@ -10,6 +10,15 @@ namespace LeetCode406
     {
         static void Main(string[] args)
         {
+            Solution s = new Solution();
+            int[] i1 = new int[2] { 7, 0 };
+            int[] i2 = new int[2] { 4, 4 };
+            int[] i3 = new int[2] { 7, 1 };
+            int[] i4 = new int[2] { 5, 0 };
+            int[] i5 = new int[2] { 6, 1 };
+            int[] i6 = new int[2] { 5, 2 };
+            List<int[]> people = new List<int[]>() { i1, i2, i3, i4, i5, i6 };
+            s.ReconstructQueue(people.ToArray());
         }
     }
 
@@ -17,13 +26,31 @@ namespace LeetCode406
     {
         public int[][] ReconstructQueue(int[][] people)
         {
-            if (people == null || people.Length == 0)
+            if (people == null)
             {
                 return null;
             }
-            people = people.OrderByDescending(o => o.First()).ToArray();
+            else if (people.Length == 0)
+            {
+                return people;
+            }
+            people = people.OrderByDescending(o => o.First()).ThenBy(o => o.Last()).ToArray();
 
+            List<int[]> res = new List<int[]>();
+            for (int i = 0; i < people.Length; i++)
+            {
+                var p = people[i];
+                if (res.Count <= p[1])
+                {
+                    res.Add(p);
+                }
+                else if (res.Count > p[1])
+                {
+                    res.Insert(p[1], p);
+                }
+            }
 
+            return res.ToArray();
         }
 
 
