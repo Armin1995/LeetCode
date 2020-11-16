f63d99ec1b3d40544b95194d6f54653c8283cc00
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-11-16 11:26:06 +0800

    128 最长连续序列

---
+++ LeetCode406/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode406
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int[][] ReconstructQueue(int[][] people)
+        {
+            if (people == null || people.Length == 0)
+            {
+                return null;
+            }
+            people = people.OrderByDescending(o => o.First()).ToArray();
+
+
+        }
+
+
+    }
+}
