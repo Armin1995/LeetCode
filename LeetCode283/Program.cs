1389e5fb3efe7c4b67f4324a0fed29c7eb383115
Armin君 <31906838+Armin1995@users.noreply.github.com>
2020-11-19 10:29:16 +0800

    283 移动零

---
+++ LeetCode283/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 移动零
+/// </summary>
+namespace LeetCode283
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int b = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[b];
+                    nums[b] = temp;
+                    b++;
+                }
+            }
+        }
+    }
+}
