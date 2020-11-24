ae61df5b5a04cb69290a0f43f16449292ba60359
Armin君 <aichenzhupo127@outlook.com>
2020-11-24 22:04:38 +0800

    222 and 452

---
+++ LeetCode222/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Solution
+    {
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int left = CountLevel(root.left);
+            int right = CountLevel(root.right);
+            if (left == right)
+            {
+                return CountNodes(root.right) + (1 << left);
+            }
+            else
+            {
+                return CountNodes(root.left) + (1 << right);
+            }
+        }
+        private int CountLevel(TreeNode root)
+        {
+            int level = 0;
+            while (root != null)
+            {
+                level++;
+                root = root.left;
+            }
+            return level;
+        }
+    }
+}
---
+++ LeetCode452/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode452
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> points = new List<int[]>() { new int[] { 10, 16 }, new int[] { 2, 8 }, new int[] { 1, 6 }, new int[] { 7, 12 } };
+
+            Solution s = new Solution();
+            s.FindMinArrowShots(points.ToArray());
+        }
+    }
+
+    public class Solution
+    {
+        public int FindMinArrowShots(int[][] points)
+        {
+            if (points == null || points.Length == 0)
+            {
+                return 0;
+            }
+            Array.Sort(points, (x1, x2) => x1[1].CompareTo(x2[1]));
+            int temp = points[0][1];
+            int result = 1;
+            foreach (var point in points)
+            {
+                if (point[0] > temp)
+                {
+                    temp = point[1];
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
