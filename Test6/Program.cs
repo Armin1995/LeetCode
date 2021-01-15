472302f31da4b630bd4f016d8991d8782192640e
Armin君 <31906838+Armin1995@users.noreply.github.com>
2021-01-15 17:08:48 +0800

    1

---
+++ Test6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    var reply = Console.ReadLine().ToUpper();
+                    if (reply == "Y")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
