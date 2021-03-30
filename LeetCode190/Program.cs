a4bd571e6b01f2f6be3fae28b15013945239a499
Armin君 <31906838+Armin1995@users.noreply.github.com>
2021-03-30 10:00:18 +0800

    190. 颠倒二进制位

---
+++ LeetCode190/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 190. 颠倒二进制位
+/// </summary>
+namespace LeetCode190
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            var n = Convert.ToUInt32("00000010100101000001111010011100", 2);
+            var r = s.reverseBits(n);
+        }
+    }
+
+    public class Solution
+    {
+        public uint reverseBits(uint n)
+        {
+            uint result = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                result += ((n & (1U << (31 - i))) != 0 ? 1U << i : 0);
+            }
+            return result;
+        }
+    }
+}
