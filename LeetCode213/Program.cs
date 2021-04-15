d85b1053349319e17853aa2e2d13466da5e70d60
Armin君 <31906838+Armin1995@users.noreply.github.com>
2021-04-15 11:02:34 +0800

    213. 打家劫舍 II

---
+++ LeetCode213/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 213. 打家劫舍 II
+/// </summary>
+namespace LeetCode213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 7, 1, 1, 9, 1, 1, 8, 1, 15, 1, 1, 7 };
+
+            Solution s = new Solution();
+            var r = s.Rob(nums);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+
+    class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            int length = nums.Length;
+            if (length == 1)
+            {
+                return nums[0];
+            }
+            else if (length == 2)
+            {
+                return Math.Max(nums[0], nums[1]);
+            }
+            return Math.Max(RobRange(nums, 0, length - 2), RobRange(nums, 1, length - 1));
+        }
+
+        public int RobRange(int[] nums, int start, int end)
+        {
+            int first = nums[start], second = Math.Max(nums[start], nums[start + 1]);
+            for (int i = start + 2; i <= end; i++)
+            {
+                int temp = second;
+                second = Math.Max(first + nums[i], second);
+                first = temp;
+            }
+            return second;
+        }
+    }
+}
