f20c0a72e3eb46b8a8d5a95535083115922f3474
Armin君 <31906838+Armin1995@users.noreply.github.com>
2021-04-25 20:03:13 +0800

    897temp

---
+++ LeetCode897/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode897
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            TreeNode parentNode = null, childNode = null;
+        }
+
+        public TreeNode GetLeftChild(TreeNode )
+        {
+
+        }
+    }
+}
